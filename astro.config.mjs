896fbcf7397a1faf9b23ae39997efb53b8abaefd
Ulysse <71192394+Ulyssedev@users.noreply.github.com>
2023-03-08 21:38:55 +0100

    💄 UI changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://curier.dev',
+    routes : [
+        { path: '/', src: 'pages/index.astro' },
+        { path: '/contact', src: 'pages/contact.astro' },
+        { path: '/projects', src: 'pages/projectsastro' },
+    ],
 });
