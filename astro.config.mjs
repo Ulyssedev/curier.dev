9d53be7a6fcb04b4e4df3c8dde119bcb099c1764
TheLowHigh <lowhigh.ytb@gmail.com>
2023-04-28 18:05:19 +0200

    🎨 Use prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://curier.dev',
-    routes : [
-        { path: '/', src: 'pages/index.astro' },
-        { path: '/contact', src: 'pages/contact.astro' },
-        { path: '/projects', src: 'pages/projectsastro' },
-    ],
-    experimental: {
-        assets: true,
-    },
+  site: 'https://curier.dev',
+  routes: [{
+    path: '/',
+    src: 'pages/index.astro'
+  }, {
+    path: '/contact',
+    src: 'pages/contact.astro'
+  }, {
+    path: '/projects',
+    src: 'pages/projectsastro'
+  }],
+  experimental: {
+    assets: true
+  },
+  integrations: [tailwind()]
 });
